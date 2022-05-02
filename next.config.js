5b05459b47fa0d42d27dfbac5dfa298ff7f02d0f
Amo Wu <amo260@gmail.com>
2022-05-02 14:00:22 +0800

    feat: 成功在 Next.js app 上運行 Unity WebGL
    基本上就是複製貼上 react-unity-webgl 的 Getting Started [1]
    
    有遇到無法載入檔案的問題，起初以為是檔名有空格的關係，看了一下是因為檔案是 GZip 的關係，參考 [2][3] 改一下 response header 就可以了
    
    [1] https://github.com/jeffreylanters/react-unity-webgl#getting-started
    [2] https://github.com/jeffreylanters/react-unity-webgl/issues/126
    [3] https://nextjs.org/docs/api-reference/next.config.js/headers
    
    Close #1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/WebGL%20Builds.data.gz',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/data',
+          },
+          {
+            key: 'Content-Encoding',
+            value: 'gzip',
+          },
+        ],
+      },
+      {
+        source: '/WebGL%20Builds.framework.js.gz',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/js',
+          },
+          {
+            key: 'Content-Encoding',
+            value: 'gzip',
+          },
+        ],
+      },
+      {
+        source: '/WebGL%20Builds.wasm.gz',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/wasm',
+          },
+          {
+            key: 'Content-Encoding',
+            value: 'gzip',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Unity, { UnityContext } from 'react-unity-webgl'
+
+const unityContext = new UnityContext({
+  loaderUrl: '/WebGL Builds.loader.js',
+  dataUrl: '/WebGL Builds.data.gz',
+  frameworkUrl: '/WebGL Builds.framework.js.gz',
+  codeUrl: '/WebGL Builds.wasm.gz',
+})
 
 const Home: NextPage = () => {
   return (
@@ -21,6 +29,8 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <Unity unityContext={unityContext} />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
